e3cc85688f7575f9e8fa609d0dc450588cefe7de
TimeBytes <aztimebytes@gmail.com>
2024-03-11 16:23:07 -0400

    inital commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
